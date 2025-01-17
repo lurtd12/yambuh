83e55b105a9fb514bc1e121944bad73165ee2ea0
lurtd12 <bypasakun4000@gmail.com>
2025-01-18 00:51:41 +0700

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// Firebase Configuration (Pastikan Anda mengganti dengan konfigurasi Firebase Anda sendiri)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
+  apiKey: "AIzaSyAIeEIuVcna2BZOiz9ImWzcq89R7l3RRSA",
+  authDomain: "menfess-x9.firebaseapp.com",
+  projectId: "menfess-x9",
+  storageBucket: "menfess-x9.firebasestorage.app",
+  messagingSenderId: "536941295630",
+  appId: "1:536941295630:web:dbdd5241c604b463c1a45f",
+  measurementId: "G-NGTPCNDP43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   
   // Ambil elemen-elemen yang dibutuhkan
   const form = document.getElementById('menfess-form');
